0564bbb07a4d1101232e07c5ee62344c915b6875
Veronica Flores <veronica.marie.flores@gmail.com>
2018-08-20 15:40:12 -0600

    push

--- index.js
+++ index.js
@@ -12,9 +12,19 @@ server.use(bp.urlencoded({
 
 
 //create an endpoint for getting a list of movies
-
+server.get('/movies', (req, res, next)=>{
+  res.send(movies)
+})
 //create an endpoint for finding a movie by its index
-
+server.get('/movies/index/:id', (req, res, next) =>{
+  let movie = movies.find(m => m.id == req.params.id)
+  if(movie){
+    return res.send(movie)
+  }
+  return res.status(400).send({
+    error: 'no movie'
+  })
+})
 //create an endpoint for finding a movie by its title
 server.get('/movies/title/:title', (req, res, next) => {
   let movie = movies.find(m => m.name == req.params.title)
@@ -27,6 +37,15 @@ server.get('/movies/title/:title', (req, res, next) => {
 })
 
 //create an endpoint for finding all movies by their years
+server.get('/movies/year/:year', (req, res, next)=>{
+  let movie = movies.find(m => m.year == req.params.year)
+  if(movie){
+    return res.send(movie)
+  }
+  return res.status(400).send({
+    error: 'no movie'
+  })
+})
 //create an endpoint for finding all by rating
 //create an endpoint for finding all by tags
 
