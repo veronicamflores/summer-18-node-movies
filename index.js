2fb0da121d9b32052ce194bdb533ccd8fd74e7a7
Jake Overall <joverall22@gmail.com>
2018-08-20 13:56:06 -0600

    example provided

--- index.js
+++ index.js
@@ -14,9 +14,21 @@ server.use(bp.urlencoded({
 //create an endpoint for getting a list of movies
 
 //create an endpoint for finding a movie by its index
+
 //create an endpoint for finding a movie by its title
+server.get('/movies/:title', (req, res, next) => {
+  let movie = movies.find(m => m.name == req.params.title)
+  if (movie) {
+    return res.send(movie)
+  }
+  return res.status(400).send({
+    error: 'no movie'
+  })
+})
+
 //create an endpoint for finding all movies by their years
 //create an endpoint for finding all by rating
+//create an endpoint for finding all by tags
 
 
 
